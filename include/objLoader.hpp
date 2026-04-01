dc7f416c49bed7f136c591239fdcf223ac10dfbd
marcper <marcos.perez22@estudiantes.uva.es>
2026-04-01 19:40:16 +0200

    .obj reader

---
+++ include/objLoader.hpp
@@ -0,0 +1,21 @@
+#ifndef OBJ_LOADER_HPP
+#define OBJ_LOADER_HPP
+
+#include <string>
+#include <vector>
+#include <array>
+
+struct Vertex {
+	std::array<float, 3> v;
+	std::array<float, 2> vt;
+
+	bool operator < (const Vertex &other) const {
+		return (v != other.v) ? v < other.v : vt < other.vt;
+	}
+};
+
+std::vector<std::string> splitLine(const std::string &line);
+std::array<int, 2> splitFace(const std::string &face);
+void readVertex(const std::string &objFile, std::vector<Vertex> &outVertices, std::vector<std::array<int, 3>> &outFaces);
+
+#endif
---
+++ src/objLoader.cpp
@@ -0,0 +1,82 @@
+#define OBJ_LOADER_IMPLEMENTATION
+#include <objLoader.hpp>
+
+#include <fstream>
+#include <sstream>
+#include <set>
+
+using namespace std;
+
+vector<string> splitLine(const string &line) {
+	vector<string> tokens;
+	istringstream iss(line);
+	string token;
+
+	while(iss >> token) {
+		tokens.push_back(token);
+	}
+	return tokens;
+}
+
+array<int, 2> splitFace(const string &face) {
+	size_t vDiv = face.find('/');
+	int vIdx = stoi(face.substr(0, vDiv));
+	int vtIdx;
+
+	if(vDiv != string::npos) {
+		size_t vtDiv = face.find('/', vDiv + 1);
+		string vtStr = face.substr(vDiv + 1, vtDiv - vDiv - 1); /* Ignore the normal vector */
+		if(!vtStr.empty()) vtIdx = stoi(vtStr);
+	}
+	
+	return { vIdx, vtIdx };
+}
+
+void readVertex(const string &objFile, vector<Vertex> &outVertices, vector<array<int, 3>> &outFaces) {
+	/* Open file */
+	ifstream file(objFile);
+	if(!file.is_open()) {
+		printf("Failed to read obj file");
+		return;
+	}
+
+	string s;
+	vector<array<float, 3>> vertices;
+	vector<array<float, 2>> texVertices;
+	set<array<int, 2>> vIndexPairs;
+
+	while(getline(file, s)) {
+		vector<string> splitted = splitLine(s);
+
+		if(splitted.empty()) continue;
+
+		string id = splitted[0];	/* v,vt,f... */
+
+		if(id == "v") {	/* v x y z */
+			vertices.push_back({ stof(splitted[1]),
+								 stof(splitted[2]),
+								 stof(splitted[3]) });
+		}
+		else if(id == "vt") { /* v x y */
+			texVertices.push_back({ stof(splitted[1]),
+								    stof(splitted[2]) });
+		}
+		else if(id == "f") { /* f v/vt/vn v/vt/vn v/vt/vn */
+			array<int, 3> face;
+			for(unsigned int i = 0; i < 3; i++) {
+				array<int, 2> pair = splitFace(splitted[i + 1]);
+
+				vIndexPairs.insert(pair);
+				face[i] = pair[0] - 1;
+			}
+			outFaces.push_back(face);
+		}
+	}
+	
+	for(auto &pair : vIndexPairs) {
+		Vertex vert;
+		vert.v = vertices[pair[0] - 1];
+		vert.vt = texVertices[pair[1] - 1];
+		outVertices.push_back(vert);
+	}
+}
